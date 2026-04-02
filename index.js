f965c797f162617750809cd8ac004cacc4679b95
Pelll27 <felisiakhe@gmail.com>
2026-04-02 23:59:07 +0700

    Update Rest Api bagian product

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ const app = http.createServer(router);
 
 app.listen(port);
 console.log(`Server running on port number: ${port}`);
+
+app.get("/", (req, res));
+res.send("Kelompok 2");
+
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = {
+    id: productId,
+    name: `Product ${productId}`,
+    price: 10 * productId,
+  };
+  res.json(product);
+});
+
+app.get("/api/products", (req, res) => {
+  const products = [
+    { id: 1, name: "Product 1", price: 10 },
+    { id: 2, name: "Product 2", price: 20 },
+    { id: 3, name: "Product 3", price: 30 },
+  ];
+  res.json(products);
+});
+
+app.put("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  res.json({ message: `Product ${productId} updated successfully` });
+});
