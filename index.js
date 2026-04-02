04844fbc610dc5ec594ed7498ad077f68d16c79a
Pelll27 <felisiakhe@gmail.com>
2026-04-03 00:16:03 +0700

    Main Program

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const app = http.createServer(router);
 
 app.listen(port);
 console.log(`Server running on port number: ${port}`);
+
+// part mongodb
+const mongoose = require("mongoose");
+
+const connectionString =
+  "mongodb://<USN>:<PASS>@ac-hpzj89u-shard-00-00.dturini.mongodb.net:27017,ac-hpzj89u-shard-00-01.dturini.mongodb.net:27017,ac-hpzj89u-shard-00-02.dturini.mongodb.net:27017/?ssl=true&replicaSet=atlas-104wvz-shard-0&authSource=admin&appName=Cluster0";
+
+mongoose.connect(connectionString);
