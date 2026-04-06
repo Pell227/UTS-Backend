ea1d119377ce746108b1c7ee38624d0ee2939a98
Azzarqyfzrn <azzarqyf@gmail.com>
2026-04-07 02:35:38 +0700

    API buat payment methods ditambahkan ke kode

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//Azzarqy Fizran M Nasrun
-//535250161
 const http = require("http");
 const router = require("./router");
 
@@ -8,6 +6,7 @@ const app = http.createServer(router);
 
 app.listen(port);
 console.log(`Server running on port number: ${port}`);
+
 
 app.get("/", (req, res));
 res.send("Kelompok 2");
@@ -35,8 +34,60 @@ app.put("/api/products/:id", (req, res) => {
   const productId = req.params.id;
   res.json({ message: `Product ${productId} updated successfully` });
 });
+
+//punya Azzarqy
+app.get("/api/payment-methods", (req, res) => {
+  res.json([
+    {id : 1, name : "Ovo", type : "e-Wallet"},
+    {id : 2, name : "Dana", type : "e-Wallet"},
+    {id : 3, name : "Gopay", type : "e-Wallet"},
+    {id : 4, name : "Mandiri", type : "Bank"},
+    {id : 5, name : "BCA", type : "Bank"},
+    {id : 6, name : "BRI", type : "Bank"}
+  ]);
+});
+
+app.put("/api/payment-methods", (req, res) => {
+  const {name, type} = req.body;
+  const newData = {
+    id : Date.now(), name, type
+  };
+
+  res.status(201).json( {
+    message : "Payment method created",
+    data : newData
+  });
+});
+
+app.put("/api/payment/methods/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.json({
+    message : `Payment method ${id} updated`,
+    data: req.body
+  });
+});
+
+app.delete("/api/payment-methods/:id", (res,  req) => {
+  const id = req.params.id;
+  
+  res.json({
+    message : `Payment method ${id} deleted`,
+  });
+});
+
+app.patch("/api/payment-methods/:id/status", (req, res) => {
+  const id = req.params.id;
+  const {isActive} = req.body;
+
+  res.json({
+    message : `Status Payment method ${id} updated`, isActive
+  })
+})
+
 // part mongodb
 const mongoose = require("mongoose");
+const { type } = require("os");
 
 const connectionString =
   "mongodb://<USN>:<PASSWORD>@ac-hpzj89u-shard-00-00.dturini.mongodb.net:27017,ac-hpzj89u-shard-00-01.dturini.mongodb.net:27017,ac-hpzj89u-shard-00-02.dturini.mongodb.net:27017/?ssl=true&replicaSet=atlas-104wvz-shard-0&authSource=admin&appName=Cluster0";
