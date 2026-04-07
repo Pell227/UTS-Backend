78dd72cbff47e4f6a0b0927ee29b8082be1f9b77
Currentlyanerd <willofvy@gmail.com>
2026-04-07 22:15:40 +0700

    Transactions

--- index.js
+++ index.js
@@ -35,6 +35,102 @@ app.put("/api/products/:id", (req, res) => {
   const productId = req.params.id;
   res.json({ message: `Product ${productId} updated successfully` });
 });
+
+//will-Transactions
+
+let transactions = [];
+let detail = [];
+
+app.post("/api/transactions", (req, res) => {
+  const { amount, type, description } = req.body;
+   if (!amount || !type) 
+  {
+    return res.status(400).json({ error: "amount and type are required" });
+  }
+
+  const newTransaction= { 
+    tanggal: new Date(),
+    id: Date.now().toString(), amount,
+    type,
+    description: description || "",
+    harga: 0,
+    subtotal: 0,
+    total: 0,
+    bayar: 0, 
+    Kembalian: 0,
+  };
+  transactions.push(newTransaction);
+  res.status(201).json(newTransaction);
+});
+
+app.get("/api/transactions", (req, res) => {
+  res.json(transactions);
+});
+
+app.post("/api/transactions/:id", (req, res) => {
+  const { id } = req.params;
+  const {id_produk, jumlah} = req.body;
+
+  const transaction = transactions.find(t => t.id == id);
+  if (!transaction) 
+  {
+    return res.status(404).json({ error: "Transaction not found" });
+  }
+  const product = products.find(p => p.id == id_produk);
+  if (!product) 
+  {
+    return res.status(404).json({ error: "Product not found" });
+  }
+  if (product.stock < jumlah) 
+  {
+    return res.status(400).json({ error: "not enough stock" });
+  }
+
+  const subtotal = product.harga * jumlah;
+
+  const detail = {
+    id_produk,
+    jumlah,
+    harga: product.price,
+    subtotal,
+  };
+
+  push(detail);
+  res.json(detail);
+});
+
+app.delete("/api/transactions", (req, res) => {
+  transactions = [];
+  res.status(204).send();
+});
+
+app.delete("/api/transactions/:id", (req, res) => {
+  const transaction = findTransaction(req.params.id);
+
+  if (!transaction) 
+    return res.status(404).json({ error: "Not found" });
+
+  transaction.deleted_at = new Date();
+
+  res.status(204).send();
+});
+
+app.get("/api/transactions/:id/total", (req, res) => {
+  const { id } = req.params;
+
+  const transaction = transactions.find(t => t.id == id);
+  
+  if (!transaction) 
+  {
+    return res.status(404).json({ error: "Transaction not found" });
+  }
+
+  const total = transaction.details.reduce((sum, detail) => sum + detail.subtotal, 0);
+  
+  res.json({ total });
+});
+
+
 // part mongodb
 const mongoose = require("mongoose");
 
