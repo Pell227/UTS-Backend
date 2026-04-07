d6783a3d94794b8366d872704ed1fec56931695e
Pelll27 <felisiakhe@gmail.com>
2026-04-07 14:31:22 +0700

    update

--- index.js
+++ index.js
@@ -84,13 +84,4 @@ app.patch("/api/payment-methods/:id/status", (req, res) => {
   res.json({
     message : `Status Payment method ${id} updated`, isActive
   })
-})
-
-// part mongodb
-const mongoose = require("mongoose");
-const { type } = require("os");
-
-const connectionString =
-  "mongodb://<USN>:<PASSWORD>@ac-hpzj89u-shard-00-00.dturini.mongodb.net:27017,ac-hpzj89u-shard-00-01.dturini.mongodb.net:27017,ac-hpzj89u-shard-00-02.dturini.mongodb.net:27017/?ssl=true&replicaSet=atlas-104wvz-shard-0&authSource=admin&appName=Cluster0";
-
-mongoose.connect(connectionString)
+});
