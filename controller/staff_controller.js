59ee199f26e1217c754eef90fa5ecfb57b590beb
Pelll27 <felisiakhe@gmail.com>
2026-04-09 20:59:26 +0700

    ye

---
+++ controller/staff_controller.js
@@ -0,0 +1,63 @@
+const Staff = require("../models/staff_model");
+
+// Get all staff
+const getAllStaff = async (req, res) => {
+  try {
+    const staff = await Staff.find();
+    res.status(200).json(staff);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get staff by ID
+const getStaffById = async (req, res) => {
+  try {
+    const staff = await Staff.findById(req.params.id);
+    if (!staff) return res.status(404).json({ message: "Staff not found" });
+    res.status(200).json(staff);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Create new staff
+const createStaff = async (req, res) => {
+  const staff = new Staff(req.body);
+  try {
+    const newStaff = await staff.save();
+    res.status(201).json(newStaff);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Update staff
+const updateStaff = async (req, res) => {
+  try {
+    const staff = await Staff.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(staff);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Delete staff
+const deleteStaff = async (req, res) => {
+  try {
+    await Staff.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Staff deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getAllStaff,
+  getStaffById,
+  createStaff,
+  updateStaff,
+  deleteStaff,
+};
