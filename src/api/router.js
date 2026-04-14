33e5be174f587814e91e81d58dbce1b4ab977cb6
Pelll27 <felisiakhe@gmail.com>
2026-04-14 20:13:13 +0700

    Updated file api for staff and report

--- src/api/router.js
+++ src/api/router.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const staff = require("./routes/staff_routes");
+const report = require("./components/Report/report_routes");
 
 module.exports = () => {
   const app = express.Router();
 
   staff(app);
+  report(app);
 
   return app;
 };
