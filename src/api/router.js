4b4441a2e876122b4db8fc80b9bd8bb8096e7a6b
Pelll27 <felisiakhe@gmail.com>
2026-04-15 12:36:15 +0700

    update

--- src/api/router.js
+++ src/api/router.js
@@ -2,12 +2,20 @@ const express = require("express");
 
 const staff = require("./routes/staff_routes");
 const report = require("./components/Report/report_routes");
+const product = require("./routes/product_routes");
+const order = require("./routes/order_routes");
+const discount = require("./routes/discount_routes");
+const payment_methods = require("./routes/payment_methods_routes");
 
 module.exports = () => {
   const app = express.Router();
 
   staff(app);
   report(app);
+  product(app);
+  order(app);
+  discount(app);
+  payment_methods(app);
 
   return app;
 };
